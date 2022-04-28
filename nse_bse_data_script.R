e0b7340796cafe0b76d09e1e356f66cb0aa46447
dhruvhaldar <haldardhruv@gmail.com>
2022-04-28 18:26:08 +0200

    Update nse_bse_data_script.R

--- nse_bse_data_script.R
+++ nse_bse_data_script.R
@@ -1,3 +1,7 @@
+####################################################################################
+#Dhruv Haldar
+#haldar@kth.se
+####################################################################################
 # Package names
 packages <- c("ggplot2", "readxl", "dplyr", "nser", "lattice", "reshape2")
 
@@ -12,10 +16,32 @@ invisible(lapply(packages, library, character.only = TRUE))
 
 # Clear Workspace 
 rm(list=ls()) 
+#####################################################################################
 
 # Actual Code Starts here
 
 fdii = fdii()
-date = fdii[1]
-gross_purchase_crores = fdii[2]
 
+#df <- data.frame(
+#  date = factor(date(fdii[3:20,1])),
+#  gross_purchase_crores = factor(gross_purchase_crores(fdii[3:20,2]))
+#)
+
+
+date = fdii[3:20,1]
+date=factor(date)
+gross_purchase_crores = fdii[3:20,2]
+gross_purchase_crores = factor(gross_purchase_crores)
+gross_purchase_crores = as.numeric(gross_purchase_crores)
+
+#Plot
+hist(gross_purchase_crores)
+
+#ggplot(aes(date, gross_purchase_crores)) +
+#geom_histogram()
+
+#plot.default(date,gross_purchase_crores,axes=TRUE, xlab = "Date", ylab= "Gross Purchase in Crores")
+#geom_histogram()
+
+### Select Rows 1 to 2
+#df[1:2,]
