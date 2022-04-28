72d92abf2a8dd1560502e947b7ee8653a51bc685
dhruvhaldar <haldardhruv@gmail.com>
2022-04-28 17:11:24 +0200

    Create nse_bse_data_script.R

---
+++ nse_bse_data_script.R
@@ -0,0 +1,11 @@
+# Package names
+packages <- c("ggplot2", "readxl", "dplyr", "nser", "lattice")
+
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+# Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
