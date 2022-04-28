5bb4c1a372e00f4a3dbe7fe4cf25ef6032025495
dhruvhaldar <haldardhruv@gmail.com>
2022-04-28 22:20:43 +0200

    Update nse_bse_data_script.R

--- nse_bse_data_script.R
+++ nse_bse_data_script.R
@@ -47,12 +47,16 @@ fdii_main <- data.frame(date,net_FII,net_DII)
 
 
 # plot the data using ggplot
-ggplot(fdii_main,
+p <- ggplot(fdii_main,
        aes(x = date, y = net_FII+net_DII)) +
-       geom_bar(stat = "identity", fill = "purple") +
+       geom_bar(stat = "identity", colour = "black", fill = "steelblue") +
+       geom_text(aes(label=net_FII+net_DII), vjust=-1.6, color="white", size=3.5)+
        scale_x_date(date_labels="%b %d",date_breaks  ="1 day") 
        labs(x = "Date",
             y = "FII and DII",
             title = "Gross Activity (Crores)",
             subtitle = "FII+DII"
-       )
+            #theme_minimal()
+            )
+# Horizontal bar plot
+#p + coord_flip()
