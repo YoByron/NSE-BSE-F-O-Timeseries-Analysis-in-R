ec41e659371fb6d8d758baeba340439cda1c5eee
dhruvhaldar <haldardhruv@gmail.com>
2022-06-04 21:52:38 +0200

    .

--- debug_script.py
+++
@@ -1,8 +0,0 @@
-#Python Nifty PE Script
-#pip install nsepy
-from datetime import date
-# Index P/E ratio history
-from nsepy import get_index_pe_history
-nifty_pe = get_index_pe_history(symbol="NIFTY",
-                                start=date(2022,1,1),
-                                end=date(2022,5,9))
